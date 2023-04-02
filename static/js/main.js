745f162a22e3d85ac83049d290b9b7b7b6bed865
Sam Pomajevich <47705880+FSP1020@users.noreply.github.com>
2023-04-01 19:05:16 -0500

    Added table and fixed mobile things

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,9 @@ var isd_query;
 var keyword_query;
 var isd_data;
 
+// Get a reference to the job table element
+const jobTable = document.getElementById("job_table");
+
 fetch('https://fsp1020.github.io/TexasEducatorJobFinderDeploy/static/data/TexasEmploymentSites_Filled.json')
   .then(res => res.json())
   .then(json => {
@@ -29,6 +32,29 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 // Initialize the marker cluster group
 var markers = L.markerClusterGroup();
 
+// Clear rows of table
+jobTable.innerHTML = "";
+var first_row = document.createElement("tr");
+var districtCol = document.createElement("td");
+districtCol.textContent = "District Name";
+var urlCol = document.createElement("td");
+urlCol.textContent = "Employment Link";
+
+first_row.appendChild(districtCol);
+first_row.appendChild(urlCol);
+
+jobTable.appendChild(first_row);
+
+// Create a class attribute:
+const att = document.createAttribute("class");
+
+// Set the value of the class attribute:
+att.value = "header-row";
+
+// Add the class attribute to the first h1:
+const tr = document.getElementsByTagName("tr")[0];
+tr.setAttributeNode(att);
+
 for (var key in X_coords) {
   var marker = L.marker([Y_coords[key], X_coords[key]]);
   marker.bindPopup(
@@ -39,6 +65,21 @@ for (var key in X_coords) {
       "</a>"
   );
   markers.addLayer(marker);
+
+  // create table row
+  var row = document.createElement("tr");
+  var districtCol = document.createElement("td");
+  districtCol.textContent = districtNames[key];
+  var urlCol = document.createElement("td");
+  var link = document.createElement("a");
+  link.textContent = isd_data["Employment_Url"][key];
+  link.href = isd_data["Employment_Url"][key];
+  urlCol.appendChild(link);
+
+  row.appendChild(districtCol);
+  row.appendChild(urlCol);
+
+  jobTable.appendChild(row);
 }
 
 // Add the marker cluster group to the map
@@ -56,6 +97,29 @@ function runCallback1() {
     // Clear the marker cluster group from the map
     markers.clearLayers();
 
+    // Clear rows of table
+    jobTable.innerHTML = "";
+    var first_row = document.createElement("tr");
+    var districtCol = document.createElement("td");
+    districtCol.textContent = "District Name";
+    var urlCol = document.createElement("td");
+    urlCol.textContent = "Employment Link";
+
+    first_row.appendChild(districtCol);
+    first_row.appendChild(urlCol);
+
+    jobTable.appendChild(first_row);
+
+    // Create a class attribute:
+    const att = document.createAttribute("class");
+
+    // Set the value of the class attribute:
+    att.value = "header-row";
+
+    // Add the class attribute to the first h1:
+    const tr = document.getElementsByTagName("tr")[0];
+    tr.setAttributeNode(att);
+
     // Initialize the marker cluster group
     markers = L.markerClusterGroup();
 
@@ -70,6 +134,22 @@ function runCallback1() {
             "</a>"
         );
         markers.addLayer(marker);
+
+        // create table row
+        var row = document.createElement("tr");
+        var districtCol = document.createElement("td");
+        districtCol.textContent = districtNames[key];
+        var urlCol = document.createElement("td");
+        var link = document.createElement("a");
+        link.textContent = isd_data["Employment_Url"][key];
+        link.href = isd_data["Employment_Url"][key];
+        urlCol.appendChild(link);
+
+
+        row.appendChild(districtCol);
+        row.appendChild(urlCol);
+
+        jobTable.appendChild(row);
       }
     }
 
@@ -84,29 +164,68 @@ input2.addEventListener("keyup", function (event) {
 });
 
 function runCallback2() {
-// console.log(keyword_query); // use keyword_query variable in the callback function
+  // console.log(keyword_query); // use keyword_query variable in the callback function
 
-// Clear the marker cluster group from the map
-markers.clearLayers();
+  // Clear the marker cluster group from the map
+  markers.clearLayers();
 
-// Initialize the marker cluster group
-markers = L.markerClusterGroup();
+  // Clear rows of table
+  jobTable.innerHTML = "";
+  var first_row = document.createElement("tr");
+  var districtCol = document.createElement("td");
+  districtCol.textContent = "District Name";
+  var urlCol = document.createElement("td");
+  urlCol.textContent = "Employment Link";
 
-for (var key in X_coords) {
-  if (employment_text[key] && employment_text[key].toLowerCase().includes(keyword_query.toLowerCase())) {
-    var marker = L.marker([Y_coords[key], X_coords[key]]);
-    marker.bindPopup(
-      "<a href='" +
-        isd_data["Employment_Url"][key] +
-        "' target='_blank'>" +
-        districtNames[key] +
-        "</a>"
-    );
-    markers.addLayer(marker);
+  first_row.appendChild(districtCol);
+  first_row.appendChild(urlCol);
+
+  jobTable.appendChild(first_row);
+
+  // Create a class attribute:
+  const att = document.createAttribute("class");
+
+  // Set the value of the class attribute:
+  att.value = "header-row";
+
+  // Add the class attribute to the first h1:
+  const tr = document.getElementsByTagName("tr")[0];
+  tr.setAttributeNode(att);
+
+  // Initialize the marker cluster group
+  markers = L.markerClusterGroup();
+
+  for (var key in X_coords) {
+    if (employment_text[key] && employment_text[key].toLowerCase().includes(keyword_query.toLowerCase())) {
+      var marker = L.marker([Y_coords[key], X_coords[key]]);
+      marker.bindPopup(
+        "<a href='" +
+          isd_data["Employment_Url"][key] +
+          "' target='_blank'>" +
+          districtNames[key] +
+          "</a>"
+      );
+      markers.addLayer(marker);
+
+      // create table row
+      var row = document.createElement("tr");
+      var districtCol = document.createElement("td");
+      districtCol.textContent = districtNames[key];
+      var urlCol = document.createElement("td");
+      var link = document.createElement("a");
+      link.textContent = isd_data["Employment_Url"][key];
+      link.href = isd_data["Employment_Url"][key];
+      urlCol.appendChild(link);
+
+
+      row.appendChild(districtCol);
+      row.appendChild(urlCol);
+
+      jobTable.appendChild(row);
+    }
   }
-}
 
-// Add the marker cluster group to the map
-map.addLayer(markers);
-}
+  // Add the marker cluster group to the map
+  map.addLayer(markers);
+  }
   })
